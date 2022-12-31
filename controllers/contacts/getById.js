c5ccca9d837d881f048edb46b3f85ab685384307
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-12-31 20:57:44 +0200

    helpers/feature
    fix status error contactSchema

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,15 +1,13 @@
-const contacts = require("../../models/contacts");
+const Contact = require("../../models/contact");
 const { HttpError } = require("../../helpers");
 
 const getById = async (req, res, next) => {
-  const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
-
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-
-  res.json(result);
+  // const { contactId } = req.params;
+  // const result = await contacts.getContactById(contactId);
+  // if (!result) {
+  //   throw HttpError(404, "Not found");
+  // }
+  // res.json(result);
 };
 
 module.exports = getById;
--- models/contact.js
+++ models/contact.js
@@ -1,21 +1,31 @@
 const { Schema, model } = require("mongoose");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const { handleMongooseError } = require("../helpers");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
 
 const Contact = model("contact", contactSchema);
 
