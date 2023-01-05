b9877cfe6063d0d1ded430e5788043ff14132329
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2023-01-05 12:56:29 +0200

    models/users/feature
    step01 - feature model users, fix model contact and fix controllers/contacts

--- controllers/contacts/removeById.js
+++ controllers/contacts/removeById.js
@@ -1,4 +1,4 @@
-const Contact = require("../../models/contact");
+const { Contact } = require("../../models");
 const { HttpError } = require("../../helpers");
 
 const removeById = async (req, res, next) => {
