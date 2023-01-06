31c01443715edba25c80a6caa0384061c2e3ebf7
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2023-01-06 09:04:13 +0200

    pagination/feature
    pagination for the contact collection

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,13 @@ const { Contact } = require("../../models");
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }).populate("owner", "email");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner }, "", {
+    skip,
+    limit,
+  }).populate("owner", "email subscription");
 
   res.json(result);
 };
