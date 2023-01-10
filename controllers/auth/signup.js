bedf957a00639d9cf85a09a11973ec4b943a7be7
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2023-01-10 19:15:35 +0200

    gravatar/feature
    step02-03 - add avatarURL use gravatar

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models");
 const { HttpError } = require("../../helpers");
@@ -12,7 +13,12 @@ const signup = async (req, res, next) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
